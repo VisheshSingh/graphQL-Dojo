0c9e4169989af0ae109a8b315cf10087d3db5a1e
Vishesh <visheshsingh16@gmail.com>
2018-12-22 17:52:42 -0500

    Book List component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import BookList from "./components/BookList";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Ninja's Readling List</h1>
+        <BookList />
       </div>
     );
   }
