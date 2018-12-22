c06b88f7e3383f1d1af43725e4e1c2b119f8bdcb
Vishesh <visheshsingh16@gmail.com>
2018-12-22 14:46:20 -0500

    Author Type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -16,6 +16,12 @@ const books = [
   { id: "2", name: "Final Empire", genre: "Fantasy" },
   { id: "3", name: "The Long Drill", genre: "Sci-fi" }
 ];
+const authors = [
+  { id: "1", name: "Patrick Adams", age: 54 },
+  { id: "2", name: "Demi Lovato", age: 22 },
+  { id: "3", name: "Adam Smith", age: 45 }
+];
+
 // SCHEMA - BOOK TYPE
 const BookType = new GraphQLObjectType({
   name: "Book",
@@ -23,6 +29,15 @@ const BookType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
+  })
+});
+// SCHEMA - AUTHOR TYPE
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt }
   })
 });
 
@@ -38,6 +53,16 @@ const RootQuery = new GraphQLObjectType({
       resolve(parent, args) {
         // Code to get data from db/ other source
         return _.find(books, { id: args.id });
+      }
+    },
+    author: {
+      type: AuthorType,
+      args: {
+        id: { type: GraphQLID }
+      },
+      resolve(parent, args) {
+        // Code to get data from db/ other source
+        return _.find(authors, { id: args.id });
       }
     }
   }
