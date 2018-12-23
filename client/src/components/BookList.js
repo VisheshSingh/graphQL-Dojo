02b460488b4ccc8a96e24f54f4e0edb26d4490b9
Vishesh <visheshsingh16@gmail.com>
2018-12-23 09:51:51 -0500

    Making a single query

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -4,19 +4,32 @@ import { getBooksQuery } from "../queries/queries";
 import BookDetails from "./BookDetails";
 
 class BookList extends Component {
+  state = {
+    selected: null
+  };
+
   displayBooks = () => {
     var data = this.props.data;
     if (data.loading) {
       return <div>Loading Books...</div>;
     } else {
-      return data.books.map(book => <li key={book.id}>{book.name}</li>);
+      return data.books.map(book => (
+        <li
+          key={book.id}
+          onClick={e => {
+            this.setState({ selected: book.id });
+          }}
+        >
+          {book.name}
+        </li>
+      ));
     }
   };
   render() {
     return (
       <div>
         <ul id="book-list">{this.displayBooks()}</ul>
-        <BookDetails />
+        <BookDetails bookId={this.state.selected} />
       </div>
     );
   }
