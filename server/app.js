3fdaf08e6dd9335fbbd9af298e34982348121e66
Vishesh <visheshsingh16@gmail.com>
2018-12-22 19:39:52 -0500

    Making queries from React

--- server/app.js
+++ server/app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
+
+//ALLOW CROSS ORIGIN REQUESTS
+app.use(cors());
 
 // connect to mLab
 mongoose.connect(
